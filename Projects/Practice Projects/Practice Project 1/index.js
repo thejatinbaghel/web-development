723a4e2166ab99603eb511f29183a8dc928e9023
Jatin Baghel <baghelj008@gmail.com>
2025-08-26 14:39:24 +0530

    Nice

---
+++ Projects/Practice Projects/Practice Project 1/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+    res.render("index");
+})
+
+app.get("/author/jatin", (req, res) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log("It's running...")
+})
