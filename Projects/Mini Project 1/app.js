8d2cc2d5ab90d69fe65f0248bf42f3a82835f4f8
Jatin Baghel <baghelj008@gmail.com>
2025-09-02 23:35:59 +0530

    Make some changes

--- Projects/Mini Project 1/app.js
+++ Projects/Mini Project 1/app.js
@@ -80,6 +80,25 @@ app.post('/post', isLoggedIn, async (req, res) => {
     res.redirect('/profile');
 })
 
+app.get('/like/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+
+    await post.save();
+    res.redirect('/profile');
+})
+
+app.get('/edit/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    res.render('edit', {post});
+})
+
 function isLoggedIn(req, res, next){
     if(req.cookies.token === "") res.send("You must be logged in!");
     else{
