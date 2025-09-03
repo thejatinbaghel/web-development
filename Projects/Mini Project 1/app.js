1fba21b5f4609933c87c977e7f2e12f6677e05fa
Jatin Baghel <baghelj008@gmail.com>
2025-09-03 23:04:07 +0530

    Multer

--- Projects/Mini Project 1/app.js
+++ Projects/Mini Project 1/app.js
@@ -5,11 +5,30 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, function (err, bytes){
+        const fn = bytes.toString("hex") + path.extname(file.originalname);
+        cb(null, fn);
+    })
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -97,6 +116,19 @@ app.get('/like/:id', isLoggedIn, async (req, res) => {
 app.get('/edit/:id', isLoggedIn, async (req, res) => {
     let post = await postModel.findOne({_id: req.params.id}).populate("user");
     res.render('edit', {post});
+})
+
+app.post('/update/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+    res.redirect("/profile");
+})
+
+app.get('/test', (req, res) => {
+    res.render("test");
+})
+
+app.post('/upload', upload.single("image"), (req, res) => {
+    console.log(req.file);
 })
 
 function isLoggedIn(req, res, next){
