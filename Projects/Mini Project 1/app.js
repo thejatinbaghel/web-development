ce3bde1a43fe361391391eeb1eb6674c2f3a00e8
Jatin Baghel <baghelj008@gmail.com>
2025-09-03 23:05:56 +0530

    Minor changes

--- Projects/Mini Project 1/app.js
+++ Projects/Mini Project 1/app.js
@@ -14,22 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/uploads')
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12, function (err, bytes){
-        const fn = bytes.toString("hex") + path.extname(file.originalname);
-        cb(null, fn);
-    })
-  }
-})
-
-const upload = multer({ storage: storage })
-
-
-
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -123,13 +107,6 @@ app.post('/update/:id', isLoggedIn, async (req, res) => {
     res.redirect("/profile");
 })
 
-app.get('/test', (req, res) => {
-    res.render("test");
-})
-
-app.post('/upload', upload.single("image"), (req, res) => {
-    console.log(req.file);
-})
 
 function isLoggedIn(req, res, next){
     if(req.cookies.token === "") res.send("You must be logged in!");
